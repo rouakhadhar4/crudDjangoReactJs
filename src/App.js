e5c7b46395c0ec90b061ad575fce892a06356fa0
khadharroua4 <khadharroua4@gmail.com>
2024-09-09 19:02:04 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import PatientList from './components/PatientList'
+
+function App() {
+  return (
+    <div className="App">
+      <PatientList></PatientList>
+     
+    </div>
+  );
+}
+
+export default App;
